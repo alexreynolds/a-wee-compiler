966a359aeb9278814e439e533cba8491944cbace
Alex Reynolds <thealexreynolds@gmail.com>
2013-05-09 09:58:43 -0400

    generatin' dat code

--- codegen.js
+++ codegen.js
@@ -20,11 +20,19 @@ function ExecEnv () {
 	// An array to hold the content of the table
 	this.content = [];
 
+	// Initialize multiple rows
+	for (var n = 0; n < 12 ; n++) {
+		this.newRow();
+	}
+
 	// Points to the row currently being edited in table
-	this.currRow = {};
+	this.currRow = this.content[0];
 
 	// Current index of last added cell
 	this.locindex = -1;
+
+	// Current index of last open space in table
+	this.tailindex = this.content.length * 8;
 
 	// Number of last row in environment
 	// Rows are numbered starting at 0 and alternate between
@@ -41,10 +49,6 @@ function ExecEnv () {
 
 		// Adds row to table content
 		this.content.push(row);
-
-		// Changes currRow to new row
-		var loc = this.content.indexOf(row);
-		this.currRow = this.content[loc];
 
 		// Increment rowindex appropriately
 		if (rowindex%10 == 0)
@@ -65,11 +69,28 @@ function ExecEnv () {
 
 		// If the row has reached maximum length (9), start a new row
 		if (this.currRow.length >= 9) {
-			this.newEntry();
+			var nextrow = this.content.indexOf(this.currRow);
+			this.currRow = this.content[nextrow];
 		}
 
 		// Increments locindex
 		this.locindex++;
+	};
+
+	// Adds a new cell of entry to the end of the table
+	this.addTailEntry = function (opcode) {
+
+		// Gets the index of where in the row the entry should go
+		var position = this.tailindex % 8;
+
+		// Gets the index of the row where the entry should go
+		var lastrow = Math.floor(this.tailindex / this.content.length);
+
+		// Adds entry to the end of the current row
+		this.content[lastrow][position] = opcode;
+
+		// Decrements tailindex
+		this.tailindex--;
 	};
 
 
@@ -262,7 +283,7 @@ function codeGen(ast) {
 	var jumpsTable = new JumpsTable();
 
 	// Tracks current scope
-	var currScope = -1;
+	var scopeCounter = -1;
 
 	// Sets current symbol table node to root
 	symbolTable.curr = symbolTable.root;
@@ -276,18 +297,10 @@ function codeGen(ast) {
   		return !isNaN(parseFloat(n)) && isFinite(n);
 	}
 
-	// === SET UP ENVIRONMENT ===
-
-	execEnv.addEntry("A9"); // Load accumulator 
-	execEnv.addEntry("00"); // Accumulator loaded with constant = 0
-	execEnv.addEntry("8D");	// Store acc into memory
-	//execEnv.addEntry("T0");	// Acc stored in loc Temp0
-	//execEnv.addEntry("XX");	// Denoted as T0XX
-
 	// In case there are no blocks of code in the AST
 	if (ast.root.name !== "block")
 	{
-		currScope = 0;
+		scopeCounter = 0;
 	}
 
 	// Traverses the AST and generates code in the process
@@ -310,11 +323,21 @@ function codeGen(ast) {
 				// If a new block of code
 				if (node.name == "block"){
 
-					currScope++;
+					scopeCounter++;
 
-					// Moves symbol table pointer down a scope
-					// RETHINK THIS
-					symbolTable.curr = symbolTable.curr.child[0];
+					// Move down to the next scope level
+					// Set to the first child by default
+					var nextScope = symbolTable.curr.children[0];
+
+					for (var i = 0; i < symbolTable.curr.children.length; i++) {
+
+						if (symbolTable.curr.children[i].scope == scopeCounter)
+						{
+							nextScope = symbolTable.curr.children[i];
+						}
+					}
+
+					symbolTable.curr = nextScope;
 
 				}
 				// If an id declaration
@@ -323,19 +346,41 @@ function codeGen(ast) {
 					var id = node.children[1].name;
 					var type = node.children[0].name;
 
+					// Construct Temp location
+					var temploc = "T";
+					var tempnum = tempAcc.toString();
+					temploc = temploc.concat(tempnum);	// temploc = "T#"
+
+					// Increment tempAcc
+					tempAcc++;
+
 					// If id is of type int
 					if (type == "int") {
 
-						// Construct Temp location
-						var temp = "T";
-						var tempnum = tempAcc.toString();
-						temp = temp.concat(tempnum);	// temp = "TX"
+						// Update execution env
 
-						// Store accumulator in Temp loc
-						execEnv.addEntry(temp);
-						execEnv.addEntry("XX");
+						execEnv.addEntry("A9"); // Load accumulator 
+						execEnv.addEntry("00"); // Accumulator loaded with constant = 0
+						execEnv.addEntry("8D");	// Store accumulator into memory
+						execEnv.addEntry(temploc);
+						execEnv.addEntry("XX"); // Store accumulator in Temp loc
+
 						// Add entry to static data table
-						staticData.addEntry(temp.concat("XX"), id, currScope);
+						staticData.addEntry(temploc.concat("XX"), id, scopelevel);
+
+					}
+					// If id is of type string
+					else if (type == "string") {
+
+						// Add entry to heap data
+						// Offset is 0 because it is unknown what the string is yet
+						heapData.addEntry(temploc.concat("XX"), id, scopelevel, 0);
+
+					}
+					// If id is of type boolean
+					else if (type == "boolean") {
+
+						// *** DO THIS LATER I HAVE NO IDEA
 					}
 
 				}
@@ -346,8 +391,9 @@ function codeGen(ast) {
 					var val = node.children[1].name;	// val to assign to id
 					var decid = {}	// Will hold id entry in table for type checking if it has been declared
 					var decid2 = {}	// Will hold second id entry (if it exists) in table for type checking if it has been declared
-					
 					var tempscope = symbolTable.curr;
+					var scopelevel = STsearch(tempscope, id);			// Gets the scope of current id
+					var temploc = staticData.getEntry(id, scopelevel);	// Gets the temporary location of the id
 					
 					// If the val to evaluate is an IntExpr, calculate value of IntExpr
 					// then store it in proper temp location
@@ -357,7 +403,7 @@ function codeGen(ast) {
 						var startnode = node.children[1];
 
 						// Second id to check if necessary
-						var id2;
+						var id2 = "";
 
 						// Accumulator, starts at LHS value
 						var acc = parseInt(startnode.children[0]);
@@ -368,13 +414,10 @@ function codeGen(ast) {
 							// Get right hand child node
 							var rhsname = startnode.children[1].name;
 
-							// If RHC is an id, set it to id to be checked
+							// If RHC is an id, get its value
 							if (!isNumber(rhsname) && rhsname.length == 1 && !(rhsname == "+" || rhsname == "-")) {
 
 								id2 = rhsname;
-
-								// *** FIGURE OUT HOW TO CHECK THE VALUE OF THE ID
-								// 		THEN ADD IT TO THE ACC
 
 							}
 							// Else RHC is a number, update acc
@@ -394,92 +437,121 @@ function codeGen(ast) {
 							startnode = startnode.children[1];
 						}
 
-							
-							var tempscope = symbolTable.curr;
-							var scopelevel = 0;
-							var found = false;
+						// Modify value to be stored so it is a 2 digit number (string)
+						if (acc < 10) {
+							val = "0".concat(acc.toString());
+						}
+						else
+						{
+							val = acc.toString
+						}
 
-							// Runs up symbol table tree to find most recent id declaration
-							while (tempscope.parent !== undefined && !found) {
+						// Add to execution environment
 
-								// Check this scope to see if id has already been declared
-								for (var i = 0; i < tempscope.ids.length; i++)
-								{
-									var currid = tempscope.ids[i].getID();
+						execEnv.addEntry("A9");	// Load accumulator
+						execEnv.addEntry(val); 	// Store value in accumulator
 
-									if (currid == id) {
-										found = true;
-										scopelevel = tempscope.scope;
-									}
-								}
+						
+						// If an id was found
+						if (id2.length > 0) {
 
-								tempscope = tempscope.parent;
-							}
+							// Reset tempscope to current scope
+							tempscope = symbolTable.curr;
 
-							// Gets the temporary location of the id
-							var temploc = staticData.getEntry(id, scopelevel);
+							var id2Scope = STsearch(tempscope, id2);				// Gets scope level of id2		
+							var id2Temploc = staticData.getEntry(id2, id2Scope);	// Gets temp location of id2
 
-							// Modify value to be stored so it is a 2 digit number (string)
-							if (acc < 10) {
-								val = "0".concat(acc.toString());
-							}
-							else
-							{
-								val = acc.toString
-							}
+							// Add id2 value to accumulator
 
-							execEnv.addEntry("A9");	// Load accumulator
-							execEnv.addEntry(val); 	// Store value in accumulator
-							execEnv.addEntry("8D");	// Save accumulator in memory
-							execEnv.addEntry(temploc.substring(0,1));
-							execEnv.addEntry(temploc.substring(2,3));	// Store accumulator in temp location
-
-
+							execEnv.addEntry("6D");							// Add value from id2 address to accumulator
+							execEnv.addEntry(id2Temploc.substring(0,1));
+							execEnv.addEntry(id2Temploc.substring(2,3));	// id2 address
 
 						}
 
-						// If assignment is of type id = id
-						else if (val.length == 1 && !isNumber(val) && !(val == "+" || val == "-")) {
+						execEnv.addEntry("8D");	// Save accumulator in memory
+						execEnv.addEntry(temploc.substring(0,1));	// Store accumulator in temp location
+						execEnv.addEntry(temploc.substring(2,3));	// 	T#XX
 
-							// THINGS
+					}
+					// If assignment is of type id = id
+					else if (val.length == 1 && !isNumber(val) && !(val == "+" || val == "-")) {
 
+						// Reset tempscope to current scope
+						tempscope = symbolTable.curr;
+
+						var valScope = STsearch(tempscope, val);				// Gets scope level of val		
+						var valTemploc = staticData.getEntry(val, valScope);	// Gets temp location of val
+
+						// Add to execution environment
+						execEnv.addEntry("AD");							// Load accumulator from memory
+						execEnv.addEntry(valTemploc.substring(0,1));
+						execEnv.addEntry(valTemploc.substring(2,3));	// Temp loc of val
+						execEnv.addEntry("8D");							// Store accumulator in id's temp location
+						execEnv.addEntry(temploc.substring(0,1));		// id's temp location
+						execEnv.addEntry(temploc.substring(2,3));		// T#XX
+
+
+					}
+					// If assignment is of type id = equal? (boolean)
+					else if (val == "equal?") {
+
+
+
+
+						///** EVALUATE EQUAL HERE
+
+
+
+					}
+					// If assignment is of type id = boolval (boolean)
+					else if (val == "true" || val == "false") {
+
+						// THINGS
+
+					}
+					// If assignment is of type id = int
+					else if (isNumber(val)) {
+
+						// Changes val from a string to an int
+						val = parseInt(val);
+
+						// Modify value to be stored so it is a 2 digit number (string)
+						if (val < 10) {
+							val = "0".concat(val.toString());
 						}
-						// If assignment is of type id = equal? (boolean)
-						else if (val == "equal?") {
+
+						// Add to execution environment
+						execEnv.addEntry("A9");	// Load accumulator
+						execEnv.addEntry(val); 	// Store value in accumulator
+						execEnv.addEntry("8D");	// Save accumulator in memory
+						execEnv.addEntry(temploc.substring(0,1));	// Store accumulator in temp location
+						execEnv.addEntry(temploc.substring(2,3));	// 	TYXX
 
 
+					}
+					// If assignment is of type id = string
+					else if (val.length > 1) {
 
+						// Access the heap data entry for the given id and update offset
+						heapData.getEntry(temploc).offset = -(val.length + 1);
 
-							///** EVALUATE EQUAL HERE
+						// Update execution environment with string at the end
+						execEnv.addTailEntry("00");	// Signifies end of string
 
-
-
+						// Steps backward through val and adds each char (in hex)
+						// to the execution environment end
+						for (var i = val.length - 1; i = 0; i--) {
+							var charHex = val.charCodeAt(i).toString(16).toUpperCase();
+							execEnv.addTailEntry(charHex);
 						}
-						// If assignment is of type id = boolval (boolean)
-						else if (val == "true" || val == "false") {
 
-							// THINGS
+					}
+					// Error
+					else {
 
-						}
-						// If assignment is of type id = int
-						else if (isNumber(val)) {
-
-							// THINGS
-
-
-						}
-						// If assignment is of type id = string
-						else if (val.length > 1) {
-
-							// Note that the id has been initialized
-							decid.initialized = true;
-
-						}
-						// Error
-						else {
-
-							// WHO KNOWS?
-						}
+						console.log("Error generating assignment code.");
+					}
 
 				}
 				// If a print statement
@@ -490,11 +562,28 @@ function codeGen(ast) {
 
 					var id = node.children[0].name;
 					var tempscope = symbolTable.curr;
+					var scopelevel = STsearch(tempscope, id);			// Gets the scope of current id
+					var temploc = staticData.getEntry(id, scopelevel);	// Gets the temporary location of the id
+					
 
 					// If the item in the print statement is just an int
 					if (isNumber(id) && node.children[0].children.length == 0) {
 
-						// THINGS
+						var constant = parseInt(id);
+
+						// Makes int a two digit string
+						if (constant < 10) {
+							constant = "0".concat(constant.toString());
+						}
+						else {
+							constant = constant.toString();
+						}
+
+						execEnv.addEntry("A0");		// Load Y register with constant
+						execEnv.addEntry(constant);	// Load constant
+						execEnv.addEntry("A2");		// Load X register with constant
+						execEnv.addEntry("01");		// Load X register with 1
+						execEnv.addEntry("FF");		// System call
 
 					}
 
@@ -508,18 +597,42 @@ function codeGen(ast) {
 					// If the item in the print statement is an empty string
 					if (id.length == 0 && node.children[0].children.length == 0) {
 						
-						// THINGS
+						// DO NOTHING, PRINT NOTHING
 
 					}
 
+					// If the item in the print statement is an id
+					if (id.length == 1 && node.children[0].children.length == 0) {
+
+						execEnv.addEntry("AC");	// Load Y register with contents of id
+						execEnv.addEntry(temploc.substring(0,1));	// At location
+						execEnv.addEntry(temploc.substring(2,3));	// temploc
+						execEnv.addEntry("A2");	// Load X register with constant
+						execEnv.addEntry("01"); // Constant = 1
+						execEnv.addEntry("FF");	// System call
+					}
+
 					// If the id being printed is an IntExpr
+					// Evaluate IntExpr, then print
 					if (id == "+" || id == "-") {
 
 						// Starting node is the current id node
 						var startnode = node.children[0];
+						// Accumulator, initialized to LHS of first level of intExpr
+						var acc = parseInt(startnode.children[0].name);
+						// Variable to hold an id, if found
+						var rhsid = "";
 
 						// Iterate through RHS
 						while (startnode.name == "+" || startnode.name == "-") {
+
+							// Get val of LHS of expr
+							var lhsval = parseInt(startnode.children[0].name);
+
+							// Adjust accumulator
+							if (startnode.name == "+") { acc += lhsval; }
+							else { acc -= lhsval; }
+
 
 							// Get right hand child node
 							var rhsname = startnode.children[1].name;
@@ -533,14 +646,52 @@ function codeGen(ast) {
 							// If RHC is an int
 							else if (isNumber(rhsname)) {
 
-
+								if (startnode.name == "+") { acc += parseInt(rhsname); }
+								else { acc -= parseInt(rhsname); }
 							}
 
 							// Next node to check is the RHC of curr node (because LHS must be a digit by grammar)
 							startnode = startnode.children[1];
 						}
-					
-						// THINGS
+
+							// Makes acc constant a two digit string
+							if (acc < 10) {
+								acc = "0".concat(acc.toString());
+							}
+							else {
+								acc = acc.toString();
+							}
+							
+							// Update execution environment
+							
+							execEnv.addEntry("A9");		// Load the accumulator with a constant
+							execEnv.addEntry(acc);		// Load acc to accumulator
+
+							// IntExpr contained an id, evaluate its value
+							if (rhsid.length > 0) {
+								// Reset tempscope to current scope
+								tempscope = symbolTable.curr;
+
+								var id2Scope = STsearch(tempscope, id2);				// Gets scope level of id2		
+								var id2Temploc = staticData.getEntry(id2, id2Scope);	// Gets temp location of id2
+
+								// Add id2 value to accumulator
+
+								execEnv.addEntry("6D");							// Add value from id2 address to accumulator
+								execEnv.addEntry(id2Temploc.substring(0,1));
+								execEnv.addEntry(id2Temploc.substring(2,3));	// id2 address
+							}
+
+							execEnv.addEntry("8D");		// Store accumulator in memory
+							execEnv.addEntry("00");
+							execEnv.addEntry("00");		// Store it in location 0000
+							execEnv.addEntry("AC");		// Load Y register from memory
+							execEnv.addEntry("00");
+							execEnv.addEntry("00");		// Load from location 0000
+							execEnv.addEntry("A2");		// Load the X register with a constant
+							execEnv.addEntry("01");		// Constant = 1
+							execEnv.addEntry("FF");		// System call
+
 
 				}
 				// If an IF or WHILE statement
@@ -664,7 +815,7 @@ function codeGen(ast) {
 					if (node.name == "block" && j == node.children.length - 1) {
 
 						symbolTable.curr = symbolTable.curr.parent;
-						
+
 					}
 				}
 
@@ -680,3 +831,29 @@ function codeGen(ast) {
 	astTraverse(ast.root, 0);
 
 }
+
+
+// Goes up through the symbol table looking for the most recent
+// occurrence of a given id
+function STsearch (startnode, id) {
+
+	var found = false;
+
+	while (startnode.parent !== undefined && !found) {
+		
+		// Check this scope to see if id has already been declared
+		for (var i = 0; i < startnode.ids.length; i++)
+		{
+			var currid = startnode.ids[i].getID();
+
+			if (currid == id) {
+				found = true;
+				return startnode.scope;
+			}
+		}
+
+		// Move up tree if not found at current scope
+		startnode = startnode.parent;
+	}
+}
+
